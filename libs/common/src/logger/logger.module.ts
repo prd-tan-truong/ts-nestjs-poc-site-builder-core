f8d8c1c1692ee2a3036ac22344f345ee998d451c
tan.truong <tan.truong@paradox.ai>
2023-11-14 01:13:17 +0700

    init: @app/common/logger with PinoLogger

---
+++ libs/common/src/logger/logger.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { LoggerModule as PinoLogger } from 'nestjs-pino';
+
+@Module({
+  imports: [
+    PinoLogger.forRoot({
+      pinoHttp: {
+        transport:
+          process.env.NODE_ENV !== 'production'
+            ? {
+                target: 'pino-pretty',
+                options: {
+                  singleLine: true,
+                },
+              }
+            : undefined,
+      },
+    }),
+  ],
+})
+export class LoggerModule {}
