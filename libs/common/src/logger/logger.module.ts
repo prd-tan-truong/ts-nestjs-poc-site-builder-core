8167a3531f7d393121e00333c1df63906f044f2b
tan.truong <tan.truong@paradox.ai>
2023-11-14 13:42:15 +0700

    feat(app-common): update logger to have requestId

--- libs/common/src/logger/logger.module.ts
+++ libs/common/src/logger/logger.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
+import { randomUUID } from 'crypto';
 import { LoggerModule as PinoLogger } from 'nestjs-pino';
 
 @Module({
   imports: [
     PinoLogger.forRoot({
       pinoHttp: {
+        genReqId: (req, res) => {
+          const id = req.headers['x-request-id'] || randomUUID();
+          res.setHeader('X-Request-ID', id);
+          return id;
+        },
         transport:
           process.env.NODE_ENV !== 'production'
             ? {
